2004a94347ea020b18b09694f83db80f6d5fcae7
tuerxunbiekebateli <batelituerxunbieke@cheyipai.com>
2022-07-04 14:06:10 +0800

    it's test1

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -73,7 +73,7 @@ class VetController {
 		// objects so it is simpler for JSon/Object mapping
 		Vets vets = new Vets();
 		vets.getVetList().addAll(this.vets.findAll());
-		System.out.println("it's a test");
+		System.out.println("it's a test, right?");
 		return vets;
 	}
 
